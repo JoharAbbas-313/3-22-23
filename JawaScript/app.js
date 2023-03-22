a99c4597fbb7d7995e7e24d46b2e00e3986d55ef
JoharAbbas-313 <123437515+JoharAbbas-313@users.noreply.github.com>
2023-03-22 15:37:06 -0700

    Add files via upload

---
+++ JawaScript/app.js
@@ -0,0 +1,56 @@
+// CURRENT getDATE AND getTIME FUNCTION 
+
+// var d = new Date();
+//  var myTime = d.getHours();
+//  document.write(myTime);
+
+// var h = new Date();
+// var myTime = h.getHours();
+// document.write(myTime);
+
+// var d = new Date();
+// var myTime = d.getHours();
+// console.log(myTime);
+
+// var d = new Date();
+// var myDate = d.getDate();
+// document.write(myDate);
+
+// var d = new Date();
+// var myDate = d.getDate();
+// console.log(myDate);
+
+// getDAY FUNCTION WITH NUMBER AND STRING 
+
+// var d = new Date();
+// var myday = d.getDay();
+// document.write(myday);
+
+// var d = new Date();
+//   var myday = d.getDay();
+//   if(myday == 0)
+//   {
+//   document.write('Sunday');
+//   } 
+ 
+//  else if (myday == 1)
+//  {
+//  document.write('Monday');
+//  }
+//  else if (myday == 2)
+//  {
+//  document.write('Tuesday');
+//  }
+//  else if (myday == 3)
+//  {
+//  document.write('Wednesday');
+//  }
+//  else if (myday == 4)
+//  {
+//  document.write('Thursday');
+//  }
+//  else if (myday == 5)
+//  {
+//  document.write('Friday');
+//  }
+//  else
